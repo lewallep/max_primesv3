f7344f71f4d1134f038d15df014d8b9d48a4dc1c
lewallep <lewallep@onid.oregonstate.edu>
2016-04-10 17:54:57 -0700

    Readme and working program

--- max_primesv3.py
+++ max_primesv3.py
@@ -8,15 +8,14 @@ class primeObject:
 		self.evalNumber = evalNumber
 		self.isPrime = isPrime
 
-def calcPrimesErastos(numProcs, calcRange, endTime, i, q):
-	print ("Calcing primes with ancient method.  Yeay Greeks!")
+def calcPrimesErastos(numProcs, calcRange, endTime, threadCount, q):
 	# This is a place holder initializer until I setup the multiple threads.
 	# At this time each individual thread will have a number which I will 
 	# multiply by the calc range to 
 
 	# This index is to help me keep track of which index I am adding which prime to
 	# Essentially all it is is a glorified counter for my lists. 
-	evalNumber = (i * calcRange) + 1	#Used to put the numbers into the lists for record keeping. Will be replaced.
+	evalNumber = (threadCount * calcRange) + 1	#Used to put the numbers into the lists for record keeping. Will be replaced.
 	#Will be used in the loops to ensure I have not gone beyond the list extents.
 		#0 * calcRange
 	listLength = 0
@@ -31,8 +30,8 @@ def calcPrimesErastos(numProcs, calcRange, endTime, i, q):
 		startIndex = 0
 		endIndex = startIndex + int(calcRange / 2)
 		evalIndex = 0
-		print ("Start Index: " + str(startIndex))
-		print ("End Index: " + str(endIndex))
+		#print ("Start Index: " + str(startIndex))
+		#print ("End Index: " + str(endIndex))
 
 		if evalNumber == 1:
 			for i in range(startIndex, endIndex - 1):
@@ -54,8 +53,8 @@ def calcPrimesErastos(numProcs, calcRange, endTime, i, q):
 		listLength = len(primeList)
 
 		highestCurNum = primeList[listLength - 1].evalNumber
-		print ("highestCurNum: " + str(highestCurNum))
-		print ("listLength: " + str(listLength))
+		#print ("highestCurNum: " + str(highestCurNum))
+		#print ("listLength: " + str(listLength))
 
 		while divisor < highestCurNum:
 			while evalIndex < listLength:
@@ -71,27 +70,31 @@ def calcPrimesErastos(numProcs, calcRange, endTime, i, q):
 		evalNumber += (numProcs * calcRange) - calcRange
 
 		if time.time() < endTime:
-			for i in range(startIndex, listLength):
-				print ("Index: " + str(i) + " evalNumber: " + str(primeList[i].evalNumber) \
-					+ " isPrime: " + str(primeList[i].isPrime))
+			#for i in range(startIndex, listLength):
+			#	print ("Index: " + str(i) + " evalNumber: " + str(primeList[i].evalNumber) \
+			#		+ " isPrime: " + str(primeList[i].isPrime))
 
-			print ("highestCurNum: " + str(highestCurNum))
-			print ("numProcs: " + str(numProcs))
+			#print ("highestCurNum: " + str(highestCurNum))
+			#print ("numProcs: " + str(numProcs))
 			# Refer list iterator
 			r = listLength - 1
+			# There seems to be a glitch with this piece of code
+			# At certain times it seems the memory cannot be accessed even though
+			# the list has the proper length.
 			while primeList[r].isPrime == False:
 				r -= 1
+			print("Highest prime this number range in thread: " + str(threadCount) \
+				+ " is: " + str(primeList[r].evalNumber))
 			q.put(primeList[r].evalNumber)
 
-			print ("The highest prime this list is: " + str(primeList[r].evalNumber))
-			print ("highestCurNum: " + str(highestCurNum))
-			print ("evalNumber: " + str(evalNumber))
-			print ("time: " + (str(time.time())))
+			#print ("The highest prime this list is: " + str(primeList[r].evalNumber))
+			#print ("highestCurNum: " + str(highestCurNum))
+			#print ("evalNumber: " + str(evalNumber))
+			#print ("time: " + (str(time.time())))
 		# End outermost while loop.
 
 
 def main(args):
-	print("The first line in my program.")
 	runTime = float(args[2])
 	calcRange = int(args[1])
 	
@@ -101,6 +104,8 @@ def main(args):
 	numProcs = mp.cpu_count()
 	timeLeft = runTime
 	highestPrimeList = []
+	highestPrime = 0
+	whileCounter = 0
 
 	q = Queue()
 
@@ -116,10 +121,25 @@ def main(args):
 		timeLeft -= 1
 
 	while not q.empty():
-		highestPrimeList.append(q.get())
+		highestPrimeList.append(q.get())	
 
-	print (highestPrimeList)
+	listLength = len(highestPrimeList)
 
+	while numProcs > whileCounter:
+		if highestPrimeList[listLength - whileCounter - 1] > highestPrime:
+			highestPrime = highestPrimeList[listLength - whileCounter - 1]
+		whileCounter += 1
+
+	print ()
+	print ("Time is up!")
+	print ()
+	print ("The highest overall prime is: " + str(highestPrime))
+	print ()
+
+	for i in range(0, numProcs):
+		p.join()
+
+	q.close()
 
 if __name__ == '__main__':
 	main(sys.argv)
